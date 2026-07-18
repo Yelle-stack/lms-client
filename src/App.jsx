4cd868d185fbf3de5e0e641979780d16a7ece845
Jelena <jelena33.bliss@gmail.com>
2026-07-18 15:19:14 +0200

    srcPackage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useMatch } from 'react-router-dom';
 
 import Home from './pages/student/Home';
 import CoursesList from './pages/student/CoursesList';
@@ -12,10 +12,16 @@ import Dashboard from './pages/educator/Dashboard';
 import AddCourse from './pages/educator/AddCourse';
 import MyCourses from './pages/educator/MyCourses';
 import StudentsEnrolled from './pages/educator/StudentsEnrollrd'
+import Navbar from './components/student/Navbar';
 
 const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*')
+
   return (
-    <div>
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute && <Navbar />}
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CoursesList />} />
