1e8db3576706212dfbd18d277151829279a4c7b9
Jelena <jelena33.bliss@gmail.com>
2026-07-24 09:46:28 +0200

    srcCallToActionHome

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -3,14 +3,16 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
 import TestimonialSection from '../../components/student/TestimonialSection'
+import CallToAction from '../../components/student/CallToAction'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7'>
       <Hero />
-      <Companies/>
-      <CoursesSection/>
-      <TestimonialSection/>
+      <Companies />
+      <CoursesSection />
+      <TestimonialSection />
+      <CallToAction />
     </div>
   )
 }
