792e7c9f71806752ef2fadbde633e202831d7ef2
Jelena <jelena33.bliss@gmail.com>
2026-07-25 14:38:59 +0200

    src

--- src/pages/student/CoursesList.jsx
+++ src/pages/student/CoursesList.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
+import SearchBar from '../../components/student/SearchBar'
+import { useParams } from 'react-router-dom'
 
 const CoursesList = () => {
+
+ const {navigate} = useContext(AppContext)
+ const {input} = useParams()
+
   return (
-    <div>
-      <h1>Courses List pages</h1>
+    <>
+    <div className='relative md:px-36 px-8 pt-20 text-left'>
+      <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
+        <div>
+          <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+          <p className='text-gray-500'>
+          <span className='text-blue-600 cursor-pointer'
+          onClick={()=> navigate('/')}>Home</span> / <span>Course List</span>
+          </p>
+        </div>
+       <SearchBar data={input}/>
+      </div>
     </div>
+    </>
   )
 }
 
