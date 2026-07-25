03f964f94fdf454382561bec781bc71dc37822b2
Jelena <jelena33.bliss@gmail.com>
2026-07-25 18:26:21 +0200

    src

--- src/pages/student/CoursesList.jsx
+++ src/pages/student/CoursesList.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
 import SearchBar from '../../components/student/SearchBar'
 import { useParams } from 'react-router-dom'
+import CourseCard from '../../components/student/CourseCard'
+import React, { useContext, useEffect, useState } from 'react'
 
 const CoursesList = () => {
 
- const {navigate} = useContext(AppContext)
+ const {navigate, allCourses} = useContext(AppContext)
  const {input} = useParams()
+ const [filteredCourse, setFilteredCourse] = useState([])
+
+ useEffect(()=>{
+   if(allCourses && allCourses.length > 0){
+    const tempCourses = allCourses.slice()
+
+    input ? 
+      setFilteredCourse(
+        tempCourses.filter(
+          item => item.courseTitle.toLowerCase().includes(input.toLowerCase())
+        )
+      )
+    : setFilteredCourse(tempCourses)
+
+   }
+ },[allCourses, input])
 
   return (
     <>
@@ -21,6 +37,9 @@ const CoursesList = () => {
           </p>
         </div>
        <SearchBar data={input}/>
+      </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
+        {filteredCourse.map((course, index)=> <CourseCard key={index} course={course}/>)}
       </div>
     </div>
     </>
