e7d3190fd4f00875ae40d96ab5ee8a402918dbfa
Jelena <jelena33.bliss@gmail.com>
2026-07-26 15:21:00 +0200

    src

--- src/pages/student/CoursesList.jsx
+++ src/pages/student/CoursesList.jsx
@@ -3,6 +3,7 @@ import SearchBar from '../../components/student/SearchBar'
 import { useParams } from 'react-router-dom'
 import CourseCard from '../../components/student/CourseCard'
 import React, { useContext, useEffect, useState } from 'react'
+import { assets } from '../../assets/assets'
 
 const CoursesList = () => {
 
@@ -38,6 +39,14 @@ const CoursesList = () => {
         </div>
        <SearchBar data={input}/>
       </div>
+
+        { input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600'>
+          <p>{input}</p>
+          <img src={assets.cross_icon} alt="" className='cursor-pointer' onClick={()=> navigate('/course-list')}/>
+        </div>
+
+        }
+
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
         {filteredCourse.map((course, index)=> <CourseCard key={index} course={course}/>)}
       </div>
