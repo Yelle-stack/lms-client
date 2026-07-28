14106a41fc6181c9671bca8f0eb609ee32698d7a
Jelena <jelena33.bliss@gmail.com>
2026-07-28 17:48:49 +0200

    srcTailwind

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -2,6 +2,8 @@
 import { useParams } from 'react-router-dom'
 import { useState, useContext, useEffect } from 'react'
 import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
+
 
 
 
@@ -22,11 +24,24 @@ const CourseDetails = () => {
      fetchCourseData()
    },[])
 
-  return (
-    <div>
-      <h1>Course Details pages</h1>
+  return courseData ? (
+    <>
+    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pr-20 text-left'>
+
+      <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
+      
+      {/* left column */}
+       <div>
+        <h1>{courseData.courseTitle}</h1>
+        <p>{courseData.courseDescription}</p>
+       </div>
+
+      {/* right column */}
+      <div></div>
+
     </div>
-  )
+    </>
+  ) : <Loading />
 }
 
 export default CourseDetails
