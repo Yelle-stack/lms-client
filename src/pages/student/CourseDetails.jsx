96127f6c6a071274b0e9bd83e950ec1d9197a808
Jelena <jelena33.bliss@gmail.com>
2026-08-02 15:50:26 +0200

    src

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -55,7 +55,30 @@ const CourseDetails = () => {
            <h2 className='text-xl font-semibold'>Course Structure</h2>
            <div className='pt-5'>
               {courseData.courseContent.map((chapter, index)=> (
-                <div></div>
+                <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
+                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
+                    <div className='flex items-center gap-2'>
+                      <img src={assets.down_arrow_icon} alt="arrow icon" />
+                      <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
+                    </div>
+                    <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
+                  </div>
+
+                  <div>
+                    <ul>
+                      {chapter.chapterContent.map((lecture, i)=> (
+                        <li key={i}>
+                          <img src={assets.play_icon} alt="play icon" className='w-4 h-4 mt-1'/>
+                          <div>
+                            <p>{lecture.lectureTitle}</p>
+                            <div></div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+
+                </div>
               ))}
            </div>
         </div>
