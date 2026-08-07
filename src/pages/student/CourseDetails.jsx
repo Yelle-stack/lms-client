45aac948c4293902007ebe5e4e06d01247f89416
Jelena <jelena33.bliss@gmail.com>
2026-08-07 19:40:11 +0200

    src

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -4,6 +4,8 @@ import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/student/Loading'
 import { assets } from '../../assets/assets'
 import humanizeDuration from 'humanize-duration'
+import Footer from '../../components/student/Footer'
+import YouTube from 'react-youtube'
 
 const CourseDetails = () => {
 
@@ -12,6 +14,7 @@ const CourseDetails = () => {
   const [courseData, setCourseData] = useState(null)
   const [openSections, setOpenSections] = useState({})
   const [isAlreadyEnrolled, setisAlreadyEnrolled] = useState(false)
+  const [playerData, setPlayerData] = useState(null)
 
   const {
     allCourses,
@@ -179,7 +182,11 @@ const CourseDetails = () => {
                               <div className='flex items-center gap-3 text-xs text-gray-500'>
 
                                 {lecture.isPreviewFree && (
-                                  <p className='text-blue-600 cursor-pointer'>
+                                  <p 
+                                  onClick={()=> setPlayerData({
+                                    videoId: lecture.lectureUrl.split('/').pop()
+                                  })}
+                                  className='text-blue-500 cursor-pointer'>
                                     Preview
                                   </p>
                                 )}
@@ -228,10 +235,19 @@ const CourseDetails = () => {
 
         {/* RIGHT COLUMN */}
         <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
-         <img src={courseData.courseThumbnail} alt="" />
-         <div className='p-5'>
-           <div className='flex items-center gap-2'>
-            <img className='w-3.5' src={assets.time_left_clock_icon} alt="time left clock icon" />
+
+          { 
+          playerData ? 
+                 <YouTube videoId={playerData.videoId} opts={{playerVars: {autoplay: 1}}} iframeClassName='w-full aspect-video'/>
+              : <img src={courseData.courseThumbnail} alt="" />
+              }
+              
+             <div className='p-5'>
+             <div className='flex items-center gap-2'>
+
+             
+              :  <img className='w-3.5' src={assets.time_left_clock_icon} alt="time left clock icon" />
+            
             <p className='text-red-500'><span className='font-medium'>5 days</span> left at this price!</p>
            </div>
 
@@ -267,10 +283,22 @@ const CourseDetails = () => {
            <button className='md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium'
            >{isAlreadyEnrolled ? 'Already Enrolled' : 'Enroll Now'}</button>
 
+           <div className='pt-6'>
+            <p className='md:text-xl text-lg font-medium text-gray-800'>What's in the course?</p>
+            <ul className='ml-4 pt-2 text-sm md:text-default list-disc text-gray-500'>
+              <li>Lifetime access with free updates.</li>
+              <li>Step-by-step, hands-on project guidance.</li>
+              <li>Downloadable resources and source code.</li>
+              <li>Quizzes to test your knowlege.</li>
+              <li>Certificate of completion.</li>
+            </ul>
+           </div>
+
          </div>
         </div>
 
       </div>
+      <Footer />
     </>
   ) : (
     <Loading />
