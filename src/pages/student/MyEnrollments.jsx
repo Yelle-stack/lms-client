acc8b45319b8e6fdc1bc85cee0194637cb4e06b4
Jelena <jelena33.bliss@gmail.com>
2026-08-08 13:42:25 +0200

    src

--- src/pages/student/MyEnrollments.jsx
+++ src/pages/student/MyEnrollments.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const MyEnrollments = () => {
   return (
     <div>
-      <h1>My Enrollments pages</h1>
+      <h1>My Enrollments</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Course</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
