b11ee3e0be207687fcdc87fee5da33ccb798fcfc
Jelena <jelena33.bliss@gmail.com>
2026-08-10 18:31:51 +0200

    src

--- src/pages/student/MyEnrollments.jsx
+++ src/pages/student/MyEnrollments.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const MyEnrollments = () => {
+
+  const {enrolledCourses, calculateCourseDuration} = useContext(AppContext)
+
   return (
-    <div>
-      <h1>My Enrollments</h1>
-      <table>
-        <thead>
+
+    <>
+    <div className='md:px-36 px-8 pt-10'>
+      <h1 className='text-2xl font-semibold'>My Enrollments</h1>
+      <table className='md:table-auto table-fixed w-full overflow-hidden border mt-10'>
+        <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left max-sm:hidden'>
           <tr>
             <th className='px-4 py-3 font-semibold truncate'>Course</th>
             <th className='px-4 py-3 font-semibold truncate'>Duration</th>
@@ -13,8 +19,24 @@ const MyEnrollments = () => {
             <th className='px-4 py-3 font-semibold truncate'>Status</th>
           </tr>
         </thead>
+        <tbody>
+          {enrolledCourses.map((course, index)=>(
+            <tr key={index}>
+              <td>
+                <img src={course.courseThumbnail} alt="" className='w-14 sm:w-24 md:w-28'/>
+                <div>
+                  <p>{course.courseTitle}</p>
+                </div>
+              </td>
+              <td>
+                {calculateCourseDuration(course)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
+    </>
   )
 }
 
