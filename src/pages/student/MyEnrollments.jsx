d0d2222a58bcc33234235ccad407818526883ddb
Jelena <jelena33.bliss@gmail.com>
2026-08-12 18:59:22 +0200

    src

--- src/pages/student/MyEnrollments.jsx
+++ src/pages/student/MyEnrollments.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState} from 'react'
 import { AppContext } from '../../context/AppContext'
+import {Line} from 'rc-progress'
+import Footer from '../../components/student/Footer'
 
 const MyEnrollments = () => {
 
-  const {enrolledCourses, calculateCourseDuration} = useContext(AppContext)
+  const {enrolledCourses, calculateCourseDuration, navigate} = useContext(AppContext)
 
   const [progressArray, setProgressArray] = useState([
     {lectureCompleted: 2, totalLectures: 4},
@@ -43,6 +45,7 @@ const MyEnrollments = () => {
                 <img src={course.courseThumbnail} alt="" className='w-14 sm:w-24 md:w-28'/>
                 <div className='flex-1'>
                   <p className='mb-1 max-sm:text-sm'>{course.courseTitle}</p>
+                  <Line strokeWidth={2} percent={progressArray[index] ? (progressArray[index].lectureCompleted * 100) / progressArray[index].totalLectures : 0 }className='bg-gray-300 rounded-full'/>
                 </div>
               </td>
               <td className='px-4 py-3 max-sm:hidden'>
@@ -52,7 +55,7 @@ const MyEnrollments = () => {
                 {progressArray[index] && `${progressArray[index].lectureCompleted} / ${progressArray[index].totalLectures}`} <span>Lectures</span>
               </td>
               <td className='px-4 py-3 max-sm:text-right'>
-                <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>
+                <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white' onClick={()=> navigate('/player/' + course._id)}>
                   {progressArray[index] && progressArray[index].lectureCompleted / progressArray[index].totalLectures === 1 ? 'Completed' : 'On Going'}
                   </button>
               </td>
@@ -61,6 +64,8 @@ const MyEnrollments = () => {
         </tbody>
       </table>
     </div>
+
+    <Footer />
     </>
   )
 }
