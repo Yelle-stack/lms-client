ff7d9e563631a2160faeaa2d4367718a5f3d5994
Jelena <jelena33.bliss@gmail.com>
2026-08-19 16:41:10 +0200

    src

--- src/components/educator/Sidebar.jsx
+++ src/components/educator/Sidebar.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 
 const Sidebar = () => {
+
+  const { isEducator} = useContext(AppContext)
+
+  const menuItems = [
+    { name: 'Dashboard', path: '/educator', icon: assets.home_icon},
+    { name: 'Add Course', path: '/educator/add-course', icon: assets.add_icon},
+    { name: 'My Courses', path: '/educator/my-courses', icon: assets.my_course_icon},
+    { name: 'Student Enrolled', path: '/educator/student-enrolled', icon: assets.person_tick_icon},
+  ];
+
   return (
     <div>
       <h1>Sidebar</h1>
