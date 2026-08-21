7d9e2b9d8a1430fa435aa9cb2269a428daa087ee
Jelena <jelena33.bliss@gmail.com>
2026-08-21 12:21:24 +0200

    src

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,17 @@ import Educator from './pages/educator/Educator';
 import Dashboard from './pages/educator/Dashboard';
 import AddCourse from './pages/educator/AddCourse';
 import MyCourses from './pages/educator/MyCourses';
-import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled';
 import Navbar from './components/student/Navbar';
 
 const App = () => {
 
-  const isEducatorRoute = useMatch('/educator/*')
+  const isEducatorRoute = useMatch('/educator/*');
 
   return (
     <div className='text-default min-h-screen bg-white'>
       {!isEducatorRoute && <Navbar />}
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CoursesList />} />
@@ -30,12 +30,14 @@ const App = () => {
         <Route path="/my-enrollments" element={<MyEnrollments />} />
         <Route path="/player/:courseId" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
-        <Route path='/educator' element={<Educator />}>
-           <Route path='educator' element={<Dashboard />}/>
-           <Route path='add-course' element={<AddCourse />}/>
-           <Route path='my-courses' element={<MyCourses />}/>
-           <Route path='student-enrolled' element={<StudentsEnrolled />}/>
+
+        <Route path="/educator" element={<Educator />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentsEnrolled />} />
         </Route>
+
       </Routes>
     </div>
   );
--- src/components/educator/Footer.jsx
+++ src/components/educator/Footer.jsx
@@ -3,8 +3,8 @@ import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-   <Footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
-     <div>
+   <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
+     <div className='flex items-center gap-4'>
       <img className='hidden md:block w-20' src={assets.logo} alt="logo" />
       <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
       <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
@@ -22,7 +22,7 @@ const Footer = () => {
         <img src={assets.instagram_icon} alt="instagram_icon" />
       </a>
      </div>
-   </Footer>
+   </footer>
   )
 }
 
