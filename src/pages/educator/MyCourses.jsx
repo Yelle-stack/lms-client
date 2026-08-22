a2597abaca95e883b254da153a5bbda22f82c06b
Jelena <jelena33.bliss@gmail.com>
2026-08-22 21:03:21 +0200

    src

--- src/pages/educator/MyCourses.jsx
+++ src/pages/educator/MyCourses.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
+import { useState } from 'react'
 
 const MyCourses = () => {
+
+  const {currency, allCourses} = useContext(AppContext)
+
+  const [courses, setCourses] = useState(null)
+  const fetchEducatorCourses = async () => {
+
+    setCourses(allCourses)
+  }
+
   return (
     <div>
       <h1>MyCourses page</h1>
