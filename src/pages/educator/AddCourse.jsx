2efe183aa466024924a9d4ea44ebe782d7c94182
Jelena <jelena33.bliss@gmail.com>
2026-08-27 16:23:11 +0200

    srcAppAddCourse

--- src/pages/educator/AddCourse.jsx
+++ src/pages/educator/AddCourse.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import uniqid from 'uniqid';
+import Quill from 'quill';
+import { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { assets } from '../../assets/assets';
 
 const AddCourse = () => {
+
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTitle, setCourseTitle] = useState(``)
+  const [coursePrice, setCoursePrice] = useState(0)
+  const [discount, setDiscount] = useState(0)
+  const [image, setImage] = useState(null)
+  const [chapters, setChapters] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [currentChapterId, setCurrentChapterId] = useState(null);
+  const [lectureDetails, setLectureDetails] = useState(
+    {
+      lectureTitle: '',
+      lectureDuration: '',
+      lectureUrl: '',
+      isPreviewFree: false,
+    }
+  )
+
+  useEffect(()=>{
+    // Initiate Quill only once
+    if (!quillRef.current && editorRef.current){
+      quillRef.current = new Quill(editorRef.current, {
+        theme: 'snow',
+      });
+    }
+  }, [])
+
   return (
-    <div>
-      <h1>Add Course</h1>
+    <div className='h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+      <form className=''>
+        <div className='flex flex-col gap-1'>
+          <p>Course Title</p>
+          <input onChange={e => setCourseTitle(e.target.value)} value={courseTitle}
+           type="text" placeholder='Type here' className='outline-none md:py-2.5 py-2 px-3 rounded border border-gray-300 focus:border-blue-500' required/>
+        </div>
+        <div className='flex flex-col gap-1'>
+         <p>Course Description</p>
+         <div ref={editorRef}></div>
+        </div>
+
+         <div className='flex items-center justify-between flex-wrap w-full max-w-xl mt-4'>
+          <div className='flex flex-col gap-1'>
+            <p>Course Price</p>
+              <input onChange={e => setCoursePrice(e.target.value)} value={coursePrice} type="number" placeholder='0' className='outline-none md:py-2.5 py-2 w-28 px-3 rounded border border-gray-500' required/>
+          </div>
+
+           <div className='flex md:flex-row flex-col items-center gap-3'>
+            <p>Course Thumbbail</p>
+            <label htmlFor="thumbnailImage" className='flex items-center gap-3'>
+              <img src={assets.file_upload_icon} alt="" className='p-3 bg-blue-500 rounded'/>
+              <input type="file" id='thumbnailImage' onChange={e => setImage(e.target.files[0])} accept='image/*' hidden />
+              <img className='max-h-10' src={image ? URL.createObjectURL(image) : ''} alt="" />
+            </label>
+           </div>
+         </div>
+
+      </form>
     </div>
   )
 }
